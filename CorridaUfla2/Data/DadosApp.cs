98266c5a4270722a93a2efffb879cfb2f979363e
HigorFelipe347 <hllgorfellpedosrells@gmail.com>
2025-12-09 00:37:03 -0300

    Refactor NextId methods to use multiline syntax
    Refactor NextId methods for better readability.

--- CorridaUfla2/Data/DadosApp.cs
+++ CorridaUfla2/Data/DadosApp.cs
@@ -1,19 +1,25 @@
-﻿using CorridaUfla2.Models;
+using CorridaUfla2.Models;
 
 namespace CorridaUfla2.Data
 {
     public static class DadosApp
     {
-        // Correção: Agora são Propriedades { get; set; } e usam new() simplificado
         public static List<Corredor> Corredores { get; set; } = new();
         public static List<Organizador> Organizadores { get; set; } = new();
         public static List<Kit> Kits { get; set; } = new();
         public static List<Corrida> Corridas { get; set; } = new();
         public static List<Inscricao> Inscricoes { get; set; } = new();
 
-        public static int NextIdUsuario() => (Corredores.Count + Organizadores.Count) + 1;
-        public static int NextIdKit() => Kits.Count + 1;
-        public static int NextIdCorrida() => Corridas.Count + 1;
-        public static int NextIdInscricao() => Inscricoes.Count + 1;
+        public static int NextIdUsuario() =>
+            Corredores.Count + Organizadores.Count + 1;
+
+        public static int NextIdKit() =>
+            Kits.Count + 1;
+
+        public static int NextIdCorrida() =>
+            Corridas.Count + 1;
+
+        public static int NextIdInscricao() =>
+            Inscricoes.Count + 1;
     }
 }
