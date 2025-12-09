9aaa4ff443265ab0f78ba538d2f9b334871f1348
HigorFelipe347 <hllgorfellpedosrells@gmail.com>
2025-12-09 00:31:12 -0300

    Refactor login and registration methods for clarity

--- CorridaUfla2/Controllers/ContaController.cs
+++ CorridaUfla2/Controllers/ContaController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using CorridaUfla2.Data;
 using CorridaUfla2.Models;
 
@@ -11,46 +11,54 @@ namespace CorridaUfla2.Controllers
         [HttpPost]
         public IActionResult Login(string email, string senha)
         {
-            var corredor = DadosApp.Corredores.FirstOrDefault(u => u.Email == email && u.Senha == senha);
+            var corredor = DadosApp.Corredores
+                .FirstOrDefault(u => u.Email == email && u.Senha == senha);
+
             if (corredor != null)
             {
                 SetSession(corredor.Id, corredor.Nome, TipoUsuario.Corredor);
                 return RedirectToAction("Index", "Home");
             }
-            var org = DadosApp.Organizadores.FirstOrDefault(u => u.Email == email && u.Senha == senha);
+
+            var org = DadosApp.Organizadores
+                .FirstOrDefault(u => u.Email == email && u.Senha == senha);
+
             if (org != null)
             {
                 SetSession(org.Id, org.Nome, TipoUsuario.Organizador);
                 return RedirectToAction("Index", "Home");
             }
+
             ViewBag.Erro = "Email ou senha incorretos";
             return View();
         }
 
         public IActionResult CadastroCorredor() => View();
+
         [HttpPost]
         public IActionResult CadastroCorredor(Corredor model)
         {
-            if (ModelState.IsValid)
-            {
-                model.Id = DadosApp.NextIdUsuario();
-                DadosApp.Corredores.Add(model);
-                return RedirectToAction("Login");
-            }
-            return View(model);
+            if (!ModelState.IsValid)
+                return View(model);
+
+            model.Id = DadosApp.NextIdUsuario();
+            DadosApp.Corredores.Add(model);
+
+            return RedirectToAction("Login");
         }
 
         public IActionResult CadastroOrganizador() => View();
+
         [HttpPost]
         public IActionResult CadastroOrganizador(Organizador model)
         {
-            if (ModelState.IsValid)
-            {
-                model.Id = DadosApp.NextIdUsuario();
-                DadosApp.Organizadores.Add(model);
-                return RedirectToAction("Login");
-            }
-            return View(model);
+            if (!ModelState.IsValid)
+                return View(model);
+
+            model.Id = DadosApp.NextIdUsuario();
+            DadosApp.Organizadores.Add(model);
+
+            return RedirectToAction("Login");
         }
 
         public IActionResult Logout()
